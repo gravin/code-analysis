d7c4a278d3512f0c5b80ec2c6b182f7940afcf42
gravin <gravingu@163.com>
2019-07-31 21:30:08 +0800

    1

---
+++ code-analysis-spring-3-2-8/src/main/java/com/codeanalysis/test/CustomTagTest.java
@@ -0,0 +1,13 @@
+package com.codeanalysis.test;
+
+import com.codeanalysis.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CustomTagTest {
+    public static void main(String[] args) {
+        ApplicationContext bf = new ClassPathXmlApplicationContext("spring/usertag.xml");
+        User user = (User) bf.getBean("testbean");
+        System.out.println(user.getUserName() + "," + user.getEmail());
+    }
+}
