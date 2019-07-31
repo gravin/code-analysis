e46a235a7b9a05ca9f5fbf10ae6b6c24bb080264
gravin <gravingu@163.com>
2019-07-31 23:39:00 +0800

    1

--- code-analysis-spring-3-2-8/src/main/java/com/codeanalysis/test/CustomTagTest.java
+++ code-analysis-spring-3-2-8/src/main/java/com/codeanalysis/test/CustomTagTest.java
@@ -1,12 +1,16 @@
 package com.codeanalysis.test;
 
 import com.codeanalysis.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class CustomTagTest {
     public static void main(String[] args) {
-        ApplicationContext bf = new ClassPathXmlApplicationContext("spring/userTag.xml");
+//        ApplicationContext bf = new ClassPathXmlApplicationContext("spring/userTag.xml");
+        BeanFactory bf=new XmlBeanFactory(new ClassPathResource("spring/userTag.xml"));
         User user = (User) bf.getBean("testbean");
         System.out.println(user.getUserName() + "," + user.getEmail());
     }
