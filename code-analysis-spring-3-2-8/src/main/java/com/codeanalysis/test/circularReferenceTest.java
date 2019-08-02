c49c427896e9502b067450d10b0e81adb4224e88
gavin <gravingu@163.com>
2019-08-02 11:28:17 +0800

    circularReferenceTest

---
+++ code-analysis-spring-3-2-8/src/main/java/com/codeanalysis/test/circularReferenceTest.java
@@ -0,0 +1,16 @@
+package com.codeanalysis.test;
+
+import com.codeanalysis.TestA;
+import com.codeanalysis.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class circularReferenceTest {
+    public static void main(String[] args) {
+//        ApplicationContext bf = new ClassPathXmlApplicationContext("spring/userTag.xml");
+        BeanFactory bf=new XmlBeanFactory(new ClassPathResource("spring/circularReferenceTest.xml"));
+        TestA testA = (TestA) bf.getBean("testA");
+        System.out.println(testA);
+    }
+}
