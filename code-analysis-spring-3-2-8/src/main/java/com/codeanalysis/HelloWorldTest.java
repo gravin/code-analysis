35444632064bc1a7a06f1a9c8032f450f0775d27
gravin <gravingu@163.com>
2019-08-18 23:46:56 +0800

    1

--- code-analysis-spring-3-2-8/src/main/java/com/codeanalysis/HelloWorldTest.java
+++ code-analysis-spring-3-2-8/src/main/java/com/codeanalysis/HelloWorldTest.java
@@ -6,13 +6,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class HelloWorldTest {
 
+    private Integer num;
+
     private HelloWorld helloWorld;
 
     public void setHelloWorld(HelloWorld helloWorld) {
         this.helloWorld = helloWorld;
     }
 
-    public void doSay(){
+    public HelloWorldTest(Integer num, HelloWorld helloWorld) {
+        this.num = num;
+        this.helloWorld = helloWorld;
+    }
+
+    public HelloWorldTest(Integer num) {
+        this.num = num;
+    }
+
+    public void doSay() {
         helloWorld.say();
     }
 }
