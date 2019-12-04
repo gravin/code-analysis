f4da19748d03ad80b8a7c363ae5f49bb3068a642
gavin <gravingu@163.com>
2019-12-04 17:39:40 +0800

    1

---
+++ code-analysis-spring-3-2-8/src/main/java/com/codeanalysis/simulator/XMLReaderSimulator.java
@@ -0,0 +1,90 @@
+package com.codeanalysis.simulator;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.util.CollectionUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class XMLReaderSimulator {
+    /**
+     * JAXP attribute used to configure the schema language for validation.
+     */
+    private static final String SCHEMA_LANGUAGE_ATTRIBUTE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
+
+    /**
+     * JAXP attribute value indicating the XSD schema language.
+     */
+    private static final String XSD_SCHEMA_LANGUAGE = "http://www.w3.org/2001/XMLSchema";
+
+
+    /**
+     * Suffix for DTD files
+     */
+    public static final String DTD_SUFFIX = ".dtd";
+
+    /**
+     * Suffix for schema definition files
+     */
+    public static final String XSD_SUFFIX = ".xsd";
+
+
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        factory.setAttribute(SCHEMA_LANGUAGE_ATTRIBUTE, XSD_SCHEMA_LANGUAGE);
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        builder.setErrorHandler(new ErrorHandler() {
+            @Override
+            public void warning(SAXParseException exception) throws SAXException {
+                exception.printStackTrace();
+            }
+
+            @Override
+            public void error(SAXParseException exception) throws SAXException {
+                exception.printStackTrace();
+            }
+
+            @Override
+            public void fatalError(SAXParseException exception) throws SAXException {
+                exception.printStackTrace();
+            }
+        });
+        builder.setEntityResolver(new EntityResolver() {
+            @Override
+            public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+                System.out.println("publicId:" + publicId + " systemId:" + systemId);
+                if (systemId != null) {
+                    if (systemId.endsWith(DTD_SUFFIX)) {
+
+                    } else if (systemId.endsWith(XSD_SUFFIX)) {
+                        Properties mappings =
+                                PropertiesLoaderUtils.loadAllProperties("META-INF/spring.schemas");
+                        Map<String, String> schemaMappings = new ConcurrentHashMap<String, String>(mappings.size());
+                        CollectionUtils.mergePropertiesIntoMap(mappings, schemaMappings);
+
+                        Resource resource = new ClassPathResource(schemaMappings.get(systemId));
+                        InputSource source = new InputSource(resource.getInputStream());
+                        source.setPublicId(publicId);
+                        source.setSystemId(systemId);
+                        return source;
+                    }
+                }
+                return null;
+            }
+        });
+        Document doc = builder.parse(new ClassPathResource("spring/beanFactoryTest.xml").getInputStream());
+        Element root = doc.getDocumentElement();
+        System.out.println(root);
+    }
+}
