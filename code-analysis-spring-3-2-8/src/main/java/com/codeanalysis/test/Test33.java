fbfcb981cad0446da99944d61805ff40babc5e78
gavin <gravingu@163.com>
2019-12-27 18:07:55 +0800

    1

--- code-analysis-spring-3-2-8/src/main/java/com/codeanalysis/test/Test33.java
+++ code-analysis-spring-3-2-8/src/main/java/com/codeanalysis/test/Test33.java
@@ -4,22 +4,37 @@ import org.springframework.util.ClassUtils;
 import org.springframework.util.MethodInvoker;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Properties;
 
 public class Test33 {
 
     public static void main(String[] args) {
+//
+//        int typeDifferenceWeight = MethodInvoker.getTypeDifferenceWeight(new Class[]{int.class}, new Object[]{1});
+//        int typeDifferenceWeight2 = MethodInvoker.getTypeDifferenceWeight(new Class[]{long.class}, new Object[]{1});
+//        System.out.println(typeDifferenceWeight);
+//        System.out.println(typeDifferenceWeight2);
+//
+//        System.out.println(int.class.isAssignableFrom(int.class));
+//        System.out.println(int.class.isAssignableFrom(long.class));
+//        System.out.println(int.class.isAssignableFrom(Integer.class));
+//        System.out.println(Integer.class.isAssignableFrom(int.class));
+//
+//        System.out.println(ClassUtils.isAssignable(long.class,int.class));
 
-        int typeDifferenceWeight = MethodInvoker.getTypeDifferenceWeight(new Class[]{int.class}, new Object[]{1});
-        int typeDifferenceWeight2 = MethodInvoker.getTypeDifferenceWeight(new Class[]{long.class}, new Object[]{1});
-        System.out.println(typeDifferenceWeight);
-        System.out.println(typeDifferenceWeight2);
+        Map<String, String> map = System.getenv();
+        for(Iterator<String> it = map.keySet().iterator(); it.hasNext();){
+            String key = it.next();
+            System.out.println(key + "=" + map.get(key));
+        }
+        System.out.println("---------------------------");
+        Properties properties = System.getProperties();
+        for(Iterator<Object> it = properties.keySet().iterator(); it.hasNext();){
+            Object key = it.next();
+            System.out.println(key + "=" + map.get(key));
+        }
 
-        System.out.println(int.class.isAssignableFrom(int.class));
-        System.out.println(int.class.isAssignableFrom(long.class));
-        System.out.println(int.class.isAssignableFrom(Integer.class));
-        System.out.println(Integer.class.isAssignableFrom(int.class));
-
-        System.out.println(ClassUtils.isAssignable(long.class,int.class));
     }
 }
