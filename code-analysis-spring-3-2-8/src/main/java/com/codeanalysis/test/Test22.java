54b02dc216ce5ebcc25e3d9cb6c5def169bd55fa
gravin <gravingu@163.com>
2020-01-10 20:21:04 +0800

    1

--- code-analysis-spring-3-2-8/src/main/java/com/codeanalysis/test/Test22.java
+++ code-analysis-spring-3-2-8/src/main/java/com/codeanalysis/test/Test22.java
@@ -24,12 +24,12 @@ public class Test22 {
 //                System.out.println("bbbbbbbbb");
 //            }
 //        }
-//        for (int i = 0; i < 3; i++) {
-//           System.out.println("outer"+i);
+        for (int i = 0; i < 3; i++) {
+           System.out.println("outer"+i);
             for (int j = 0; j < 5; j++) {
                 System.out.println("inner"+j);
             }
-//        }
+        }
         for (int i = 0; i < 6; i++) {
             System.out.println("inner 2"+i);
         }
