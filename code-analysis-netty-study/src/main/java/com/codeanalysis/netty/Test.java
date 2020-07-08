cbbf75581204d1449527c1d4c0123c1c5c212a83
gravin <gravingu@163.com>
2020-07-08 11:28:27 +0800

    1

---
+++ code-analysis-netty-study/src/main/java/com/codeanalysis/netty/Test.java
@@ -0,0 +1,23 @@
+package com.codeanalysis.netty;
+
+/**
+ * @author Gavin
+ * @date 2020/7/7
+ */
+public class Test {
+    private class InnerTest {
+        public void hello() {
+            System.out.println("aaaaaaaaaa");
+        }
+    }
+
+    public void Hello() {
+        InnerTest innerTest = new InnerTest();
+        innerTest.hello();
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.Hello();
+    }
+}
