1d0245371e0584ec68caf9c17a063e984942f757
gravin <gravingu@163.com>
2020-07-14 01:49:34 +0800

    1

--- code-analysis-ifeve-pdf/src/main/java/com/codeanalysis/ifeve/Main.java
+++ code-analysis-ifeve-pdf/src/main/java/com/codeanalysis/ifeve/Main.java
@@ -25,7 +25,7 @@ public class Main {
 
         Document document = DocumentUtil.getDocument(html, new HashMap<>());
         List<String> idsToRemove = Arrays.asList("header", "bread_crumb", "comments_wrapper", "previous_next_post_single"
-                , "container", "right_col", "footer"
+                , "container", "right_col", "footer", "wpstats", "google_esf"
         );
         for (String idToRemove : idsToRemove) {
             Element header = document.getElementById(idToRemove);
@@ -46,6 +46,43 @@ public class Main {
             }
         }
 
+        List<String> keywords = Arrays.asList("e-202029", "admin-ajax"
+        );
+        for (String keyword : keywords) {
+            Elements scripts = document.getElementsByTag("script");
+            if (scripts != null) {
+                for (Element script :
+                        scripts) {
+                    if (script != null) {
+                        String src = script.attr("src") + script.data();
+                        if (StringUtils.contains(src, keyword)) {
+                            script.remove();
+                        }
+                    }
+                }
+            }
+        }
+
+        String style1 = "width:100%;margin:0;padding:0;";
+        String style2 = "width:100%;";
+        String style3 = "width:95%;";
+
+        document.getElementById("main_content").attr("style", style1);
+        document.getElementById("left_col").attr("style", style1);
+
+        document.getElementsByClass("post_wrap").attr("style", style3);
+        document.getElementsByClass("post").attr("style", style2);
+        document.getElementsByClass("title").attr("style", style2);
+        document.getElementsByClass("post_content").attr("style", style2);
+        document.getElementsByClass("title").get(0).getElementsByTag("span").attr("style", style2);
+
+        Elements p = document.getElementsByTag("p");
+        for (int i = 0; i < Math.min(p.size(), 5); i++) {
+            Element e = p.get(i);
+            if (e != null && StringUtils.startsWithAny(e.text(), "原文：", "译者：", "原文链接", "作者：")) {
+                e.remove();
+            }
+        }
         DocumentUtil.write(html, document);
 
 
@@ -54,23 +91,23 @@ public class Main {
 
     private static void replaceHtml(File html) throws IOException {
         String content = FileUtils.readFileToString(html, "UTF-8");
-        content = content.replace("id=\"header\"", "id=\"header\" hidden")
-                .replace("id=\"bread_crumb\"", "id=\"bread_crumb\" hidden")
-                .replace("id=\"bread_crumb\"", "id=\"bread_crumb\" hidden")
-                .replace("class=\"meta\"", "class=\"meta\" hidden")
-                .replace("id=\"comments_wrapper\"", "id=\"comments_wrapper\" hidden")
-                .replace("id=\"previous_next_post_single\"", "id=\"previous_next_post_single\" hidden")
-                .replace("id=\"container\"", "id=\"container\" hidden")
-                .replace("id=\"right_col\"", "id=\"right_col\" hidden")
-                .replace("id=\"footer\"", "id=\"footer\" hidden")
-                .replace("<div id=\"main_content\" class=\"clearfix\">", "<div id=\"main_content\" class=\"clearfix\" style=\"width:95%;margin:0;padding:0\">")
-                .replace("<div id=\"left_col\">", "<div id=\"left_col\" style=\"width:95%;margin:0;padding:0\">")
-                .replace("<div class=\"post_wrap clearfix\" id=\"single\">", "<div class=\"post_wrap clearfix\" id=\"single\" style=\"width:95%;\">")
-                .replace("<div class=\"post\">", "<div class=\"post\" style=\"width:95%;\">")
-                .replace("<h3 class=\"title\"><span>","<h3 class=\"title\" style=\"width:95%;\"><span style=\"width:95%;\">")
-                .replace("<div class=\"post_content\">", "<div class=\"post_content\" style=\"width:95%;\">")
-
-        ;
+//        content = content.replace("id=\"header\"", "id=\"header\" hidden")
+//                .replace("id=\"bread_crumb\"", "id=\"bread_crumb\" hidden")
+//                .replace("id=\"bread_crumb\"", "id=\"bread_crumb\" hidden")
+//                .replace("class=\"meta\"", "class=\"meta\" hidden")
+//                .replace("id=\"comments_wrapper\"", "id=\"comments_wrapper\" hidden")
+//                .replace("id=\"previous_next_post_single\"", "id=\"previous_next_post_single\" hidden")
+//                .replace("id=\"container\"", "id=\"container\" hidden")
+//                .replace("id=\"right_col\"", "id=\"right_col\" hidden")
+//                .replace("id=\"footer\"", "id=\"footer\" hidden")
+//                .replace("<div id=\"main_content\" class=\"clearfix\">", "<div id=\"main_content\" class=\"clearfix\" style=\"width:95%;margin:0;padding:0\">")
+//                .replace("<div id=\"left_col\">", "<div id=\"left_col\" style=\"width:95%;margin:0;padding:0\">")
+//                .replace("<div class=\"post_wrap clearfix\" id=\"single\">", "<div class=\"post_wrap clearfix\" id=\"single\" style=\"width:95%;\">")
+//                .replace("<div class=\"post\">", "<div class=\"post\" style=\"width:95%;\">")
+//                .replace("<h3 class=\"title\"><span>", "<h3 class=\"title\" style=\"width:95%;\"><span style=\"width:95%;\">")
+//                .replace("<div class=\"post_content\">", "<div class=\"post_content\" style=\"width:95%;\">")
+//
+//        ;
 
 
         int i = content.indexOf("<div style=\"margin-top: 15px; font-style: italic\">\n" +
