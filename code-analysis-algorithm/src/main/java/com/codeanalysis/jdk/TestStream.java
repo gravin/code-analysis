3ebf2f5bcbb960b59e804c716ec3ad2fc495801b
gravin <gravingu@163.com>
2020-09-06 04:25:33 +0800

    1

--- code-analysis-algorithm/src/main/java/com/codeanalysis/jdk/TestStream.java
+++ code-analysis-algorithm/src/main/java/com/codeanalysis/jdk/TestStream.java
@@ -2,6 +2,8 @@ package com.codeanalysis.jdk;
 
 
 import com.codeanalysis.jdk.stream.Collectors;
+import com.codeanalysis.jdk.stream.IntStream;
+import com.codeanalysis.jdk.stream.Stream;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,28 +15,18 @@ import java.util.List;
 public class TestStream {
 
     public static void main(String[] args){
-        List<String> teamIndia = Arrays.asList("11", "12", "13");
-        List<String> teamAustralia = Arrays.asList("21", "22", "23","24");
-        List<String> teamEngland = Arrays.asList("31", "32", "33");
-        List<String> teamNewZeland = Arrays.asList("41", "42", "43");
-        List<String> teamSouthAfrica = Arrays.asList("52", "53");
+        // 生成关于list的流
+        Stream<Integer> intStream = IntStream.range(1,6).boxed();
+        // intStream基础上过滤出偶数的流
+//        Stream<Integer> filterStream =  intStream.filter(item-> item%2 == 0);
+        // filterStream基础上映射为平方的流
+//        Stream<Integer> mapStream = filterStream.map(item-> item * item);
+        // mapStream基础上截取前三个的流
+        Stream<Integer> limitStream = intStream.limit(2);
 
-        List<List<String>> playersInWorldCup2016 = new ArrayList<>();
-        playersInWorldCup2016.add(teamIndia);
-        playersInWorldCup2016.add(teamAustralia);
-        playersInWorldCup2016.add(teamEngland);
-        playersInWorldCup2016.add(teamNewZeland);
-        playersInWorldCup2016.add(teamSouthAfrica);
+        // 最终结果累加求和(初始值为0)
+        Integer sum = limitStream.reduce(0,(i1,i2)-> i1+i2);
 
-        // Now let's do this in Java 8 using FlatMap
-        playersInWorldCup2016.stream()
-            .flatMap(pList -> pList.stream())
-                .filter(item->item.endsWith("4"))
-                .limit(3)
-                .peek(System.out::println)
-                .collect(Collectors.toList())
-                .forEach(System.out::println);
-
-        Runnable r = ()->{};
+        System.out.println(sum); // 20
     }
 }
